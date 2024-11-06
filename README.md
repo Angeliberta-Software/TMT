4de00ba2e8cf439061762e81fa69fbc2f69d9f02
Vladimir Suhhorukov <vladimirsuhorukov@gmail.com>
2024-11-06 13:33:17 +0200

    add basic navigation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TMT
 An NVDA plugin for Meta Trader 5
+Currently this is a super pre alpha testing version. Bugs are included.
+## Requirements
+ Meta Trader 5 x64
+## Basic Navigation
+###Focus
+* Control+1: toolbox window.
+* Control+2: data window.
+* Control+3: navigator window.
+* Control+4: market watch window.
+* Control+5: workspace.
+Appropriate checkboxes should be checked in the View menu.
+Market watch window is currently very poorly accessible, but it is possible, for example, to call contract specification via context menu.
+###Announce
+* Control+P: current profile.
+* Control+0: terminal current time.
+* NVDA+T: announces active chart.
--- appModules/terminal64/__init__.py
+++ appModules/terminal64/__init__.py
@@ -1,7 +1,70 @@
 import appModuleHandler
-import tones
+import api
+import scriptHandler
+from scriptHandler import script
+from logHandler import log
+from ui import message
 
 class AppModule(appModuleHandler.AppModule):
 
-	def event_gainFocus(self, obj, nextHandler):
-		tones.beep(256, 200)
+	#def __init__(self, *args, **kwargs):
+		#super().__init__(*args, **kwargs)
+
+	def getElements(self):
+		return api.getForegroundObject().children
+
+	@script(description="Announce current profile", gesture="kb:control+p")
+	def script_announceCurrentProfile(self, gesture):
+		for o in self.getElements():
+			if o.role == 27: # TODO Find correct enumeration in NVDA docs
+				message(o.children[1].name)
+				break
+
+	@script(gesture="kb:nvda+t")
+	def script_fixWindowTitle(self, gesture):
+		titleElements = api.getForegroundObject().name.split("-")
+		msg = titleElements[len(titleElements) - 1].strip()
+		message(msg)
+
+	@script(description = "Focus on toolbox window", gesture="kb:control+1")
+	def script_focusOnToolboxWindow(self, gesture):
+		for obj in self.getElements():
+			if obj.name == "Toolbox":
+				obj.children[0].children[2].setFocus()
+				break
+
+	@script(description="Focus on data window", gesture="kb:control+2")
+	def script_focusOnDataWindow(self, gesture):
+		for obj in self.getElements():
+			if obj.name == "Navigator":
+				obj.children[0].children[0].setFocus()
+				break
+
+	@script(description="Focus on navigator window", gesture="kb:control+3")
+	def script_focusOnNavigatorWindow(self, gesture):
+		for obj in self.getElements():
+			if obj.name == "Navigator":
+				obj.children[1].children[1].setFocus()
+				break
+
+	@script(description="Focus on market watch window", gesture="kb:control+4")
+	def script_focusOnMarketWatchWindow(self, gesture):
+		for obj in self.getElements():
+			if obj.name == "Navigator":
+				obj.children[2].children[0].setFocus()
+				break
+
+	@script(description="Focus on Workspace", gesture="kb:control+5")
+	def script_focusOnWorkspace(self, gesture):
+		for obj in self.getElements():
+			if obj.name == "Workspace":
+				obj.setFocus()
+				break
+
+	@script(description="Announce terminal time", gesture="kb:control+0")
+	def script_announceTerminalTime(self, gesture):
+		for obj in self.getElements():
+			if obj.name == "Navigator":
+				nameElements = obj.children[2].name.split(": ")
+				message(nameElements[1])
+				break
