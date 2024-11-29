a236cbe71aee6ef139153d1bf566c129da7dc68a
Vladimir Suhhorukov <vladimirsuhorukov@gmail.com>
2024-11-29 10:58:12 +0200

    add logging (#1)

--- ChartReader/Server.py
+++ ChartReader/Server.py
@@ -1,5 +1,9 @@
 import socket
 from _thread import start_new_thread
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 class Server:
 
@@ -16,21 +20,22 @@ class Server:
 				connection, _ = self.server_socket.accept()
 				data = connection.recv(1024)
 				message = data.decode()
-				print("Received message: " + message)
+				logger.info("Received message: " + message)
 				if not self.callback == None:
 					self.callback(message)
 				connection.send("ok".encode())
 				connection.close()
 			except Exception as e:
-				print("Failed to serve a connection", str(e))
+				logger.error("Failed to serve a connection.", exc_info=True)
 
 	def start(self):
 		self.server_socket.listen(1)
 		start_new_thread(Server.__processConnection, (self, ))
-		print("Server started.")
+		logger.info("Server started.")
 
 	def stop(self):
 		self.server_socket.close()
+		logger.info('Server stopped.')
 
 # Example of use
 """
